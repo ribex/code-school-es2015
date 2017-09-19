fb093b0121d73da1bbf37a67eed79e4e63ddf953
Rebecca Wagaman <ribexy@gmail.com>
2017-09-19 03:15:03 +0000

    Initial commit ES2015 course

---
+++ const-vs-let.js
@@ -0,0 +1,19 @@
+function reportAbuse(topicId, throttler){
+
+  const MAX_REPORTS_PER_MINUTE = 30;
+
+  if(throttler.recentAbuseReports > MAX_REPORTS_PER_MINUTE){
+    return false;
+  }
+
+  _reportTopic(topicId, function(topic){
+  let message = "Topic " + topic.name;
+    if(topic.isReported){
+       message += " reported.";
+      _displaySuccess(message);
+    }else{
+       message += " encountered an error!";
+      _displayError(message);
+    }
+  });
+}
